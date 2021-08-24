2fef78e9d220824c43ddd6d607cc57c870bdbee6
Yuri Troshchanka <ystas78@bk.ru>
2021-08-24 09:16:55 +0300

    добавлено добавление, удаление, лайк фото

--- scripts/index.js
+++ scripts/index.js
@@ -28,99 +28,149 @@ const initialCards = [
 
 //ниже - выборка DOM элементов - сохранение их в переменные
 const profile = document.querySelector('.profile');
-const popup = document.querySelector('.popup');//попап обоих попапов - общий класс
+const popup = document.querySelector('.popup');//попап обоих попапов - общий класс //? убрать видимо надо...
 const popupProfile = document.querySelector('.popup_profile');//попап редактирование профиля
 const popupElements = document.querySelector('.popup_elements');//попап редактирование фото и их названий
 const popupProfileOpenButton = profile.querySelector('.profile__edit-button');//кнопка - открывает попап редактирование профиля
-const popupElementsAddButton = profile.querySelector('.profile__add-button');//кнопка - открывает попап редактирование фото и их названий
-const popupCloseButton = popup.querySelector('.popup__close');//кнопка закрытия обоих попапов - одна на два попапа
+const popupElementsAddButton = profile.querySelector('.profile__add-button');//кнопка - открывает попап добавления фото и их названий
+const popupCloseButtonProfile = popupProfile.querySelector('.popup__close');//кнопка закрытия попапа 
+const popupCloseButtonElements = popupElements.querySelector('.popup__close');//кнопка закрытия 
+
+const addFormFoto = popupElements.querySelector('.form');// форма добовления нового фото
+
+const popupFormNameFotoInput = popupElements.querySelector('.form__input_type_name-foto');//форма ввода - название нового фото
+const popupFormLinkFotoInput = popupElements.querySelector('.form__input_type_link-foto');//форма ввода - ссылка на новое фото
+
+
 const profileName = profile.querySelector('.profile__name');//поле профиля - фамилия-имя и т.п.
 const profileJob = profile.querySelector('.profile__job');//поле профиля - сфера деятельности,работа и т.п.
 const popupForm = popup.querySelector('.form');//общий для двух поапапов блок содержащий формы ввода данных
 const popupFormNameInput = popup.querySelector('.form__input_type_name');//форма ввода фамилии-имени и т.п.
 const popupFormJobInput = popup.querySelector('.form__input_type_job');//форма ввода сферы деятельности,работа и т.п.
-const popupFormNameFotoInput = popup.querySelector('.form__input_type_name-foto');//форма ввода - название нового фото
-const popupFormLinkFotoInput = popup.querySelector('.form__input_type_link-foto');//форма ввода - ссылка на новое фото
+
+
 const elementTemplate = document.querySelector('.template').content;//темплейт элемент - блок карточки с фото его названием
-const elementBox = document.querySelector('.elements');//то куда помещен темплейт - в <section class="elements">
+const elementBoxTemplate = document.querySelector('.elements');//то куда помещен темплейт - в <section class="elements">
 
-const elementLikeButton = elementTemplate.querySelector('.element__place_like-button');//кнопка лайка фото - сердечко
+const elementLikeActiv = document.querySelector('.element__place_like-button_active');//элемент - активный лайк фото - черный
 
-const popupFoto = document.querySelector('.popup_open-foto');//попап открытой для просмотра фотографии
-const popupCloseFotoButton = popupFoto.querySelector('.popup__close_foto-button'); //кнопка закрытия открытой фотографии
-//!-------------------------------------------------------------------------------------------------------------------------------------
+const popupOpenFoto = document.querySelector('.popup_open-foto');//тут и ниже элементы попапа открытой для просмотра фотографии
+const popupContainerFoto = popupOpenFoto.querySelector('.popup__container-foto'); 
+const popupCloseButtonFoto = popupOpenFoto.querySelector('.popup__close_foto-button'); 
+const	popupFoto = popupOpenFoto.querySelector('.popup__foto');
+const	popupTitleFoto = popupOpenFoto.querySelector ('.popup__title-foto');
 
 
-//!-------------------------------------------------------------------------------------------------------------------------------------
+//--------------------------------------------------------------------------------------------------------------------------------------
+// Функция - вешает слушатели на кнопки карточки с фото - удаление, лайк, само фото как кнопка (в выборки дом-элементов они не нужны)
+function placeEventListener(createPlace) {
+  createPlace.querySelector('.element__place_delite-button').addEventListener('click', placeDelete);//удаление
+  createPlace.querySelector('.element__place_like-button').addEventListener('click', likePlace);//лайк
+  //createPlace.querySelector('.element__image').addEventListener('click', openPopupImage);//само фото как кнопка попапа
+}
+//---------------------------------------------------------------------------------------------------------------------------------------
 //ниже - наполнение элементов для карточек с фото
-function getPlace(name, link) {
+function getPlace(elementData) {
   const createPlace = elementTemplate.cloneNode(true); //клониурем содержимое тега тепмлейт
   const placeTitle = createPlace.querySelector('.element__place-title');
   const placeImage = createPlace.querySelector('.element__image');
-  placeTitle.textContent = name;
-  placeImage.alt = name;
-  placeImage.src = link; 
+  const placeLikeButton = createPlace.querySelector('.element__place_like-button');
+  placeTitle.textContent = elementData.name;
+  placeImage.alt = elementData.name;
+  placeImage.src = elementData.link; 
   placeEventListener(createPlace);
   return createPlace;
 }
 
-initialCards.forEach(function (el) {
-  elementBox.append(getPlace(el.name, el.link)); 
+initialCards.forEach(function (elementData) {
+  elementBoxTemplate.append(getPlace(elementData)); 
 });
 
-//! Ниже - удаление карточки ----------------------------------------------------------------------------------------------------------- 
-// Сначала на кнопки карточки с картинкой вешаем слушатели (вверху в выборки дом-элементов они не нужны)
-function placeEventListener(createPlace) {
-  createPlace.querySelector('.element__place_delite-button').addEventListener('click', placeDelete);
+//добавляем новую карточку
+const addNewPlace = (event) => {
+  event.preventDefault()
+  const newPlace  = {
+    name: popupFormNameFotoInput.value, 
+    link: popupFormLinkFotoInput.value
+  }
+  elementBoxTemplate.prepend(getPlace(newPlace));
+  closePopup(popupElements);
+  event.target.reset();
 }
 
+
+
+//Ниже - функция лайка карточки с фото -----------------------------------------------------------------------------------------------------
+function likePlace(event) {
+  const placeLikeButton = event.target.closest('.element__place_like-button');
+  placeLikeButton.classList.toggle('element__place_like-button_active')
+ }
+
+// Ниже - функция удаление карточки с фото ------------------------------------------------------------------------------------------------- 
 function placeDelete (event) {
   const createPlace = event.target.closest('.element');
-  
   createPlace.remove();
 }
 
-//!-------------------------------------------------------------------------------------------------------------------------------------
-//ниже - открываем попап создав openPopup и добавляя попапу класс popup_opened в html разметку
-const openPopup = function() {
-  popup.classList.add('popup_opened');
+
+
+
+//открытие попапов
+const openPopup = function(selectedPopup) {
+  selectedPopup.classList.add('popup_opened');
+}
+
+// Ниже - открываем попап редактирования профиля (попап №1) добавляя попапу класс popup_opened в html разметку -------------------------
+const openPopupProfile = function(selectedPopup) {
   popupFormNameInput.value = profileName.textContent; //вставляем текст
-  popupFormJobInput.value = profileJob.textContent;           
+  popupFormJobInput.value = profileJob.textContent;
+  openPopup(selectedPopup);           
 }
 
-//ниже - регистрируем обработчик события на конпку открытия попапа, при клике на нее сказали - открой попап. 
-popupProfileOpenButton.addEventListener('click', openPopup);
-popupElementsAddButton.addEventListener('click', openPopup);
-
-
-//ниже - закрываем попап создав closePopup и удаляя - popup_opened из html разметки
-const closePopup = function() {
-  popup.classList.remove('popup_opened');
+//закрытие попапов
+const closePopup = function(selectedPopup) {
+  selectedPopup.classList.remove('popup_opened');
 }
 
-//ниже - регистрируем обработчик события на конпку закрытия попапа, при клике на нее сказали - закрой попап. 
-popupCloseButton.addEventListener('click', closePopup);
+
 
 //ниже - закрытие попапа нажатием мимо него, если цель (клик мышкой) сделан не на папапе - закрыть попап
-const closePopupOnOverlay = function(event) {
-  if (event.target !== event.currentTarget) {
-    return
-  }
-  closePopup()
+const onoverlay = document.querySelectorAll('.popup')
+function closePopupOnOverlay() {
+  onoverlay.forEach((popup) => {
+    popup.addEventListener('click', (evt) => {
+      if (evt.target.classList.contains('popup_opened')) {
+          closePopup(popup)
+      }
+    })
+  })
 }
-
-//ниже - регистрируем обработчик события клик мимо попапа - закрой попап. 
-popup.addEventListener('click', closePopupOnOverlay)
-//!-----------------------------------------------------------------------------------------------------------------------------------
+closePopupOnOverlay()
 
 
 
 
-const addTextProfile = function(event) {
-  event.preventDefault()
-  profileName.textContent = popupFormNameInput.value
-  profileJob.textContent = popupFormJobInput.value
-  closePopup()
+
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------
+// ниже - сохранение информации в попапе №1
+const changeTextProfile = function(event) {
+  event.preventDefault();
+  profileName.textContent = popupFormNameInput.value;
+  profileJob.textContent = popupFormJobInput.value;
+  closePopup(popupElements);
 }
+//----------------------------------------------------------------------------------------------------------------------------------
 
-popupForm.addEventListener('submit', addTextProfile)//регистрация обработчика события
+popupProfileOpenButton.addEventListener('click', () => openPopupProfile(popupProfile));
+popupElementsAddButton.addEventListener('click', () => openPopup(popupElements));
+//ниже - регистрируем обработчик события на конпку закрытия попапа, при клике - вызываем функцию закрытия. 
+popupCloseButtonProfile.addEventListener('click', () => closePopup(popupProfile));
+popupCloseButtonElements.addEventListener('click', () => closePopup(popupElements));
+
+
+popupForm.addEventListener('submit', changeTextProfile)//регистрация обработчика события
+
+addFormFoto.addEventListener('submit', addNewPlace);
+
